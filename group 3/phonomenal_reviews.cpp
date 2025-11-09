f95c5db365493107a31f9ef4facf583e9d8a3e6e
normal-newby <nimo.r.liu@gmail.com>
2025-11-09 15:58:33 -0500

    pho

--- group 3/phonomenal_reviews.cpp
+++ group 3/phonomenal_reviews.cpp
@@ -1,19 +1,29 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-void dfs(vector<vector<int>>& adjs, vector<int>& lengths, int cur){
+int nodes;
+void prune(vector<int>& phos, vector<int>& keep, vector<vector<int>>& adjs, int cur, int par){
+    if (phos[cur]) keep[cur] = 1;
     for (int adj : adjs[cur]){
-        if (lengths[adj] != -1) continue;
+        if (adj == par) continue;
+        prune(phos, keep, adjs, adj, cur);
+        if (keep[adj]) keep[cur] = 1;
+    }
+    nodes += keep[cur];
+}
+
+void dfs(vector<vector<int>>& adjs, vector<int>& lengths, int cur, vector<int>& keep){
+    for (int adj : adjs[cur]){
+        if (lengths[adj] != -1 || keep[adj] == 0) continue;
         lengths[adj] = lengths[cur] + 1;
-        dfs(adjs, lengths, cur);
+        dfs(adjs, lengths, adj, keep);
     }
 }
-pair<int, int> findLongestAndIdx(vector<int>& lengths, set<int>& phos){
+pair<int, int> findLongestAndIdx(vector<int>& lengths, vector<int>& phos, vector<int>& keep){
     int ml = 0;
     int idx;
     for (int i = 0; i < lengths.size(); i++){
-        if (!phos.count(i)) continue;
+        if (keep[i] == 0) continue;
         if (lengths[i] > ml){
             ml = lengths[i];
             idx = i;
@@ -26,34 +36,34 @@ int main(){
     cin.tie(0);
     int n, m; cin >> n >> m;
     vector<vector<int>> adjs(n);
-    set<int> phos;
-    int start;
+    vector<int> phos(n);
     for (int i = 0; i < m; i++){
         int a; cin >> a;
-        phos.insert(a);
-        start = a;
+        phos[a] = true;
     }
     for (int i = 0; i < n-1; i++){
         int a, b; cin >> a >> b;
         adjs[a].push_back(b);
         adjs[b].push_back(a);
     }
-    vector<int> lengths(m, -1);
+    vector<int> keep(n);
+    int start;
+    for (int i = 0; i < n; i++) if(phos[i]){start = i; break;}
+    keep[start] = 1;
+    prune(phos, keep, adjs, start, -1);
+
+
+    vector<int> lengths(n, -1);
 
     lengths[start] = 0;
-    dfs(adjs, lengths, start);
-    auto[_, idx] = findLongestAndIdx(lengths, phos);
+    dfs(adjs, lengths, start, keep);
+    auto[_, idx] = findLongestAndIdx(lengths, phos, keep);
     fill(lengths.begin(), lengths.end(), -1);
 
     lengths[idx] = 0;
-    dfs(adjs, lengths, start);
-    auto[longest, finish] = findLongestAndIdx(lengths, phos);
-
-    int totalLength = 0;
-    for (int length : lengths) totalLength += length;
-    totalLength*=2;
-    totalLength -= longest;
+    dfs(adjs, lengths, idx, keep);
+    auto[longest, finish] = findLongestAndIdx(lengths, phos, keep);
     
-    cout << totalLength << endl;
+    cout << 2*(nodes-1) - longest << endl;
     return 0;
 }
